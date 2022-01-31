400ad408d25f1c70291a4901aa3dd61cca513f36
Collin Thornton <thorntoncollin1@gmail.com>
2022-01-31 09:10:20 -0600

    testing

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 
 void setup() {
-  // test 
+  // test for branch
+  hello()
   // put your setup code here, to run once:
-}
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
